94d1c57a12a0f12120f51f21d19f25eec1f4245d
AntoineC35 <olivier.charlet@neuf.fr>
2024-02-07 10:25:06 +0000

    11h

--- models/Comment.php
+++ models/Comment.php
@@ -7,5 +7,79 @@
 
 class Comment
 {
+    private ? int $id = null;
 
+    /**
+     * @param string $content
+     * @param string $userId
+     * @param string $postId
+     */
+    public function __construct(private string $content, private string $userId, private string $postId)
+    {
+
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    /**
+     * @param string $userId
+     */
+    public function setUserId(?int $userId): void
+    {
+        $this->userId = $userId;
+    }
+   
+    /**
+     * @return string
+     */
+    public function getPostId(): ?int
+    {
+        return $this->postId;
+    }
+
+    /**
+     * @param string $postId
+     */
+    public function setPostId(?int $postId): void
+    {
+        $this->postId = $postId;
+    }
 }
--- models/Post.php
+++ models/Post.php
@@ -7,5 +7,113 @@
 
 class Post
 {
+    private ? int $id = null;
 
+    /**
+     * @param string $title
+     * @param string $excerpt
+     * @param string $content
+     * @param string $author
+     * @param string $createdAt
+     */
+    public function __construct(private string $title, private string $excerpt, private string $content, private string $author, private string $createdAt)
+    {
+
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int|null $id
+     */
+    public function setId(?int $id): void
+    {
+        $this->id = $id;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle(string $title): void
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getExcerpt(): string
+    {
+        return $this->excerpt;
+    }
+
+    /**
+     * @param string $excerpt
+     */
+    public function setExcerpt(string $excerpt): void
+    {
+        $this->excerpt = $excerpt;
+    }
+
+    /**
+     * @return string
+     */
+    public function getContent(): string
+    {
+        return $this->content;
+    }
+
+    /**
+     * @param string $content
+     */
+    public function setContent(string $content): void
+    {
+        $this->content = $content;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getAuthor(): ?int
+    {
+        return $this->author;
+    }
+
+    /**
+     * @param string $author
+     */
+    public function setAuthor(?int $author): void
+    {
+        $this->author = $author;
+    }
+   
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
 }
